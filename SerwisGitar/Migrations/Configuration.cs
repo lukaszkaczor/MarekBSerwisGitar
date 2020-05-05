4cba359632abd7697fa6897cec9fc86aeedc6564
Łukasz <lukaszk9396@gmail.com>
2020-05-05 13:07:06 +0200

    Initial commit

---
+++ SerwisGitar/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace SerwisGitar.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SerwisGitar.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "SerwisGitar.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(SerwisGitar.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ SerwisGitar/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SerwisGitar.Startup))]
+namespace SerwisGitar
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
